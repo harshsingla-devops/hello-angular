f53146936f61dba34d8c6f9f3dbaf3b1d8ccab62
Harsh Singla <harshsingla@Harshs-MacBook-Pro.local>
2021-09-30 22:49:03 -0400

    Added code for event and property bindings

--- project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  Output,
+  EventEmitter,
+} from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
-  styleUrls: ['./shopping-edit.component.css']
+  styleUrls: ['./shopping-edit.component.css'],
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild('ingredientName')
+  name!: ElementRef;
+  @ViewChild('IngredientAmount')
+  amount!: ElementRef;
 
-  constructor() { }
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onAddItem() {
+    let addedIngredient = new Ingredient(
+      this.amount.nativeElement.value,
+      this.name.nativeElement.value
+    );
+    this.ingredientAdded.emit(addedIngredient);
   }
-
 }
