24334f6c6277d44eef4c180ad6a59801996e358b
Harsh Singla <harshsingla@Harshs-MacBook-Pro.local>
2021-10-03 22:58:27 -0400

    Added HostBinding Decorator to set styles of the hosting element

--- directives/src/app/better-highlight/better-highlight.directive.ts
+++ directives/src/app/better-highlight/better-highlight.directive.ts
@@ -4,14 +4,16 @@ import {
   Renderer2,
   OnInit,
   HostListener,
+  HostBinding,
 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]',
 })
 export class BetterHighlightDirective implements OnInit {
+  @HostBinding('style.backgroundColor') backgroundColor = 'transparent';
+  @HostBinding('style.color') color = 'black';
   constructor(private elRef: ElementRef, private renderer: Renderer2) {}
-
   ngOnInit() {
     // this.renderer.setStyle(
     //   this.elRef.nativeElement,
@@ -22,20 +24,24 @@ export class BetterHighlightDirective implements OnInit {
   }
 
   @HostListener('mouseenter') mouseenter() {
-    this.renderer.setStyle(
-      this.elRef.nativeElement,
-      'background-color',
-      'blue'
-    );
-    this.renderer.setStyle(this.elRef.nativeElement, 'color', 'white');
+    // this.renderer.setStyle(
+    //   this.elRef.nativeElement,
+    //   'background-color',
+    //   'blue'
+    // );
+    this.backgroundColor = 'blue';
+    // this.renderer.setStyle(this.elRef.nativeElement, 'color', 'white');
+    this.color = 'white';
   }
 
   @HostListener('mouseleave') mouseleave() {
-    this.renderer.setStyle(
-      this.elRef.nativeElement,
-      'background-color',
-      'transparent'
-    );
-    this.renderer.setStyle(this.elRef.nativeElement, 'color', 'black');
+    // this.renderer.setStyle(
+    //   this.elRef.nativeElement,
+    //   'background-color',
+    //   'transparent'
+    // );
+    // this.renderer.setStyle(this.elRef.nativeElement, 'color', 'black');
+    this.backgroundColor = 'transparent';
+    this.color = 'black';
   }
 }
