c2976be2ca162cbbbb089d1d4376dca27146b174
Harsh Singla <harshsingla@Harshs-MacBook-Pro.local>
2021-10-04 12:17:54 -0400

    Injected logging service to various components

---
+++ services/src/app/account/account.component.ts
@@ -0,0 +1,24 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { LoggingServiceService } from '../services/logging-service.service';
+
+@Component({
+  selector: 'app-account',
+  templateUrl: './account.component.html',
+  styleUrls: ['./account.component.css'],
+})
+export class AccountComponent {
+  @Input() account: { name: string; status: string };
+  @Input() id: number;
+  @Output() statusChanged = new EventEmitter<{
+    id: number;
+    newStatus: string;
+  }>();
+
+  constructor(private loggingService: LoggingServiceService) {}
+
+  onSetTo(status: string) {
+    this.statusChanged.emit({ id: this.id, newStatus: status });
+    // console.log('A server status changed, new status: ' + status);
+    this.loggingService.logStatusChange(status);
+  }
+}
