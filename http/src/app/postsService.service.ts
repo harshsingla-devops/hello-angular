9c373b174ef0e551ff2e7869b41d0804ec0459a4
Harsh Singla <harshsingla@Harshs-MacBook-Pro.local>
2021-11-02 11:00:13 -0400

    Added Env variables to store API END Points

--- http/src/app/postsService.service.ts
+++ http/src/app/postsService.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map } from 'rxjs/operators';
 import { Post } from './models/post.model';
+import { environment } from '../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class PostsService {
@@ -9,33 +10,23 @@ export class PostsService {
 
   public createPost(title: string, content: string) {
     const postData = { title: title, content: content };
-    // return this.http.post('http://localhost:3000/api/v1/blogs', postData);
-    return this.http.post(
-      ' https://angular-project-d720a-default-rtdb.firebaseio.com/posts.json',
-      postData
-    );
+    return this.http.post(environment.API_END_POINT, postData);
   }
 
   public fetchPosts() {
-    return this.http
-      .get(
-        'https://angular-project-d720a-default-rtdb.firebaseio.com/posts.json'
-      )
-      .pipe(
-        map((responseData: { [key: string]: Post }) => {
-          const postsArray: Post[] = [];
-          for (const key in responseData) {
-            if (responseData.hasOwnProperty(key))
-              postsArray.push({ ...responseData[key], id: key });
-          }
-          return postsArray;
-        })
-      );
+    return this.http.get(environment.API_END_POINT).pipe(
+      map((responseData: { [key: string]: Post }) => {
+        const postsArray: Post[] = [];
+        for (const key in responseData) {
+          if (responseData.hasOwnProperty(key))
+            postsArray.push({ ...responseData[key], id: key });
+        }
+        return postsArray;
+      })
+    );
   }
 
   public deletePosts() {
-    return this.http.delete(
-      'https://angular-project-d720a-default-rtdb.firebaseio.com/posts.json'
-    );
+    return this.http.delete(environment.API_END_POINT);
   }
 }
--- http/src/environments/environment.ts
+++ http/src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  API_END_POINT:
+    'https://angular-project-d720a-default-rtdb.firebaseio.com/posts.json',
 };
 
 /*
