6b81aa9b955dd01231a547036ab34c4d0c161a0b
Harsh Singla <harshsingla@Harshs-MacBook-Pro.local>
2021-11-08 21:56:21 -0500

    added children in the routes array

--- routing/src/app/app.module.ts
+++ routing/src/app/app.module.ts
@@ -13,12 +13,20 @@ import { ServersService } from './servers/servers.service';
 import { RouterModule, Routes } from '@angular/router';
 
 const appRoutes: Routes = [
-  { path: 'servers', component: ServersComponent },
-  { path: 'servers/:id', component: ServerComponent },
-  { path: 'servers/:id/edit', component: EditServerComponent },
-  { path: 'users', component: UsersComponent },
-  { path: 'users/:id/:name', component: UserComponent },
   { path: '', component: HomeComponent },
+  {
+    path: 'servers',
+    component: ServersComponent,
+    children: [
+      { path: ':id', component: ServerComponent },
+      { path: ':id/edit', component: EditServerComponent },
+    ],
+  },
+  {
+    path: 'users',
+    component: UsersComponent,
+    children: [{ path: ':id/:name', component: UserComponent }],
+  },
 ];
 
 @NgModule({
