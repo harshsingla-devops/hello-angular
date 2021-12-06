73129b158674e34b19b86aeeb58803d586167bcb
Harsh Singla <harshsingla@Harshs-MacBook-Pro.local>
2021-12-05 22:28:20 -0500

    Added Error Handling in the POST reauest in Service Layer

--- authentication/src/app/auth/auth.component.ts
+++ authentication/src/app/auth/auth.component.ts
@@ -1,7 +1,46 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-auth',
-  templateUrl: './auth.component.html'
+  templateUrl: './auth.component.html',
 })
-export class AuthComponent {}
+export class AuthComponent {
+  isLoginMode = true;
+  isLoading = false;
+  error: string = null;
+
+  constructor(private authService: AuthService) {}
+
+  onSwitchMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
+  onSubmit(form: NgForm) {
+    // console.log(
+    //   `Email is : ${form.value.email} and password is : ${form.value.password}`
+    // );
+    if (!form.valid) return;
+    this.isLoading = true;
+    const email = form.value.email;
+    const password = form.value.password;
+
+    if (this.isLoginMode) {
+      //TODO
+    } else {
+      this.authService.signUp(email, password).subscribe(
+        (data) => {
+          this.isLoading = false;
+          console.log(data);
+        },
+        (errorMessage) => {
+          this.isLoading = false;
+          this.error = errorMessage;
+          console.log(errorMessage);
+        }
+      );
+
+      form.reset();
+    }
+  }
+}
---
+++ authentication/src/app/auth/auth.service.ts
@@ -0,0 +1,48 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+
+interface AuthResponse {
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+}
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  constructor(private http: HttpClient) {}
+
+  signUp(email: string, password: string) {
+    return this.http
+      .post<AuthResponse>(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAdfIGdYX66nAncp1ADsW_6pZ_p_1eS82o',
+        {
+          email: email,
+          password: password,
+          returnSecureToken: true,
+        }
+      )
+      .pipe(
+        catchError((errorResponse) => {
+          let errorMessage = 'An unknown Error Occured!';
+          if (!errorResponse.error || !errorResponse.error.error)
+            return throwError(errorMessage);
+          switch (errorResponse.error.error.message) {
+            case 'EMAIL_EXISTS':
+              errorMessage =
+                'The email address is already in use by another account.';
+            case 'OPERATION_NOT_ALLOWED':
+              errorMessage = 'Password sign-in is disabled for this project.';
+            case 'TOO_MANY_ATTEMPTS_TRY_LATER':
+              errorMessage =
+                'We have blocked all requests from this device due to unusual activity. Try again later.';
+          }
+          return throwError(errorMessage);
+        })
+      );
+  }
+}
