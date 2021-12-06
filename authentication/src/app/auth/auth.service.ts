87b609db9e61396b238d6573e68b8d69adecba27
Harsh Singla <harshsingla@Harshs-MacBook-Pro.local>
2021-12-05 23:04:39 -0500

    Removed API KEY

--- authentication/src/app/auth/auth.service.ts
+++ authentication/src/app/auth/auth.service.ts
@@ -20,7 +20,7 @@ export class AuthService {
   signUp(email: string, password: string) {
     return this.http
       .post<AuthResponse>(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAdfIGdYX66nAncp1ADsW_6pZ_p_1eS82o',
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[API_KEY]',
         {
           email: email,
           password: password,
@@ -33,7 +33,7 @@ export class AuthService {
   login(email: string, password: string) {
     return this.http
       .post<AuthResponse>(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAdfIGdYX66nAncp1ADsW_6pZ_p_1eS82o',
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=[API_KEY]',
         {
           email: email,
           password: password,
